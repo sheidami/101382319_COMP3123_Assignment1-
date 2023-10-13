b79fb4090cc9ba34fbc6b92b2b5c292a78cb84f6
sheidami <sheydami98@gmail.com>
2023-10-13 02:57:30 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require("mongoose")
+const user = require('./routes/user');
+const employee = require('./routes/employee');
+const app = express();
+const SERVER_PORT = 3000; 
+
+// Middleware JSON parsing
+app.use(express.json());
+app.use(express.urlencoded())
+const DB_CONNECTION_STRING = "mongodb+srv://admin:829682@cluster0.mznpgmx.mongodb.net/comp3123_assigment1?retryWrites=true&w=majority"
+mongoose.connect(DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+
+app.use('/api/v1/user', user);
+app.use('/api/v1/emp', employee);
+
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is listening on port ${SERVER_PORT}`);
+});
+
